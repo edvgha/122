3707d61a21564577d8a3f95952882ff5c3c3a590
Edvard <edvard_gh@yahoo.com>
2017-03-17 14:31:24 -0700

    decode2

--- p3/decode2.c
+++ p3/decode2.c
@@ -2,4 +2,11 @@ int decode2(int x, int y, int z);
 
 int decode2(int x, int y, int z)
 {
+    int eax, edx;
+    edx = (y - z);
+    eax = edx;
+    eax <<= 31;
+    eax >>= 31;
+    eax *= x;
+    return edx ^ eax;
 }
