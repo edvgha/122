e35a82eccf3eb2e4f7bb31a6dded72b236c4e460
edvard <edvard_gh@yahoo.com>
2017-03-23 22:20:18 -0700

    more prob

---
+++ p3/union_ele.c
@@ -0,0 +1,15 @@
+union ele {
+    struct {
+        int* p;
+        int y;
+    } e1;
+    struct {
+        int x;
+        union ele* next;
+    } e2;
+};
+
+void proc(union ele* up)
+{
+    up -> e2.next -> e1.y = *(up -> e2.next -> e1.p) - up -> e2.x;
+}
