1a78a02df8d7d674690b030487c5f5e9ec748635
Edvard Ghazaryan <gedvard@EMB-Pro.local>
2018-01-13 00:09:31 -0800

    x

--- EPI/14_8.cxx
+++ EPI/14_8.cxx
@@ -13,6 +13,14 @@ struct BSTNode
 	std::unique_ptr<BSTNode<T1, T2>> right;
 };
 
+void print(std::unique_ptr<BSTNode<int, char>>& n)
+{
+    if (!n) return ;
+    std::cout << n->data1 << std::endl;
+    print(n->left);
+    print(n->right);
+}
+
 void print_inorder(std::unique_ptr<BSTNode<int, char>>& n)
 {
 	if (!n ) 
@@ -27,7 +35,9 @@ void print_inorder(std::unique_ptr<BSTNode<int, char>>& n)
 		} else {
 			cur = stack.top();
 			stack.pop();
-			std::cout << cur -> data1 << std::endl;
+            if (cur -> data1 != 0) {
+                std::cout << cur -> data1 << std::endl;
+            }
 			cur = cur -> right.get();
 		}
 	}
@@ -35,37 +45,20 @@ void print_inorder(std::unique_ptr<BSTNode<int, char>>& n)
 
 std::unique_ptr<BSTNode<int, char>> build_min_height_bst_from_sorted_array(const std::vector<int>& arr, int start, int end)
 {
-	std::cout << start << " " << end << " --> " << (start + (end - start)/2) << std::endl;
-	/*if (start >= end ) {
-		std::unique_ptr<BSTNode<int, char>> node = std::make_unique<BSTNode<int, char>>();
-		node -> data1 = arr[start];
-		node -> data2 = 'x';
-		return node;
-	}
+	if (end == start) {
+        return std::make_unique<BSTNode<int, char>>();
+    }
 	std::unique_ptr<BSTNode<int, char>> node = std::make_unique<BSTNode<int, char>>();
 	node -> data1 = arr[start + (end - start) / 2];
 	node -> data2 = 'x';
-	node -> left = std::move(build_min_height_bst_from_sorted_array(arr, start, start + (end - start) / 2));
-	node -> right = std::move(build_min_height_bst_from_sorted_array(arr, start + (end - start)/ 2 + 1, end));
-	return node;*/
-	if (end <= start) {
-		std::unique_ptr<BSTNode<int, char>> node = std::make_unique<BSTNode<int, char>>();
-		node -> data1 = 0;
-		node -> data2 = 'x';
-		return node;
-	} else {
-	std::unique_ptr<BSTNode<int, char>> node = std::make_unique<BSTNode<int, char>>();
-	node -> data1 = arr[start + (end - start) / 2];
-	node -> data2 = 'x';
-	node -> left = std::move(build_min_height_bst_from_sorted_array(arr, start, start + (end - start + 1) / 2 - 1));
-	node -> right = std::move(build_min_height_bst_from_sorted_array(arr, start + (end - start + 1) / 2 + 1, end));
+    node -> left = std::move(build_min_height_bst_from_sorted_array(arr, start, (start + end) / 2));
+    node -> right = std::move(build_min_height_bst_from_sorted_array(arr, (start + end) / 2 + 1, end));
 	return node;
-}
 }
 
 int main()
 {
-	std::vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15};
-	auto node = build_min_height_bst_from_sorted_array(arr, 0, arr.size() - 1);
+	std::vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+	auto node = build_min_height_bst_from_sorted_array(arr, 0, arr.size());
 	print_inorder(node);
 }
