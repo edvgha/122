bdf02eec677a0a10f4315f667faee990f2323b26
Edvard Ghazaryan <edvard@EMBA.local>
2018-05-19 00:02:51 -0700

    cpp17

---
+++ cpp17/sync_cout.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+struct pcout : public stringstream {
+    static inline mutex cout_mutex;
+    ~pcout() {
+        lock_guard<mutex> l {cout_mutex};
+        cout << rdbuf();
+        cout.flush();
+    } 
+};
+
+static void print_pcout(int id)
+{
+    pcout{} << "pcout hello from " << id << '\n'; 
+}
+
+int main() {
+    vector<thread> v;
+    for (size_t i {0}; i < 10; ++i) {
+        v.emplace_back(print_pcout, i);
+    }
+    for (auto &t : v) { t.join(); }
+}
