5a938ab569493d80390e62d49f5937fc37d44712
Edvard <edvard_gh@yahoo.com>
2018-06-16 15:13:19 -0700

    13

--- EPI/13_11.cxx
+++ EPI/13_11.cxx
@@ -1,1 +1,33 @@
-TODO
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+int find_cap(std::vector<int>& arr, int payroll)
+{
+    std::sort(arr.begin(), arr.end(), [](int a, int b) { return a > b; });
+    int payroll_old = 0;
+    for (auto el : arr)
+        payroll_old += el;
+
+    int sum = 0;
+    for (size_t i = 0; i < arr.size(); ++i) 
+    {
+        sum += arr[i];
+        if (sum < (payroll_old - payroll))
+            continue;
+        auto t = (sum - (payroll_old - payroll)) / (i + 1);
+        if (t > arr[i + 1])
+            return t;
+    }
+    assert(false);
+    return 0;
+}
+
+int main()
+{
+    int payroll = 210;
+    std::vector<int> arr = {90, 30, 100, 40, 20, 120, 150};
+    int cap = find_cap(arr, payroll);
+    std::cout << "cap : " << cap << std::endl;
+}
--- EPI/13_3.cxx
+++ EPI/13_3.cxx
@@ -1,1 +1,29 @@
-TODO
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+void uniquiefy(std::vector<std::pair<std::string, std::string>>& arr)
+{
+    std::sort(arr.begin(), arr.end(), [](std::pair<std::string, std::string> a, std::pair<std::string, std::string> b) { return a.first < b.first; });
+    size_t last_unique_id = 0;
+    for (size_t i = 0; i < arr.size(); ++i) 
+    {
+        if (arr[i].first == arr[last_unique_id].first)
+            continue;
+        std::swap(arr[i], arr[++last_unique_id]);
+    }
+
+    ++last_unique_id;
+    std::cout << last_unique_id << " " << arr.size() << std::endl;
+    for (size_t i = 0; i < last_unique_id; ++i)
+        std::cout << "[" << arr[i].first << ", " << arr[i].second << "]\n";
+    std::cout << std::endl;
+}
+
+int main()
+{
+    //std::vector<std::pair<std::string, std::string>> arr = {{"Ian", "Bell"}, {"David", "Gower"}, {"David", "Gower"}, {"Ian", "Botham"}, {"Jone", "Smith"}, {"Mike", "Mike"}};
+    std::vector<std::pair<std::string, std::string>> arr = {{"a", "a"}, {"c", "c"}, {"c", "c"}, {"d", "d"}, {"e", "e"}, {"d", "d"}, {"i", "i"}};
+    uniquiefy(arr);
+}
